9356a930d2724a80df4e5557f52c56468d72510c
Thomas Grimes <tgrimes.westshoresolutions@gmail.com>
2018-03-08 13:47:36 -0500

    Update

--- src/api-server/posts.js
+++ src/api-server/posts.js
@@ -6,7 +6,7 @@ const defaultData = {
   "8xf0y6ziyjabvozdd253nd": {
     id: '8xf0y6ziyjabvozdd253nd',
     timestamp: 1467166872634,
-    title: 'Udacity is the best place to learn React',
+    title: 'Post Title here',
     body: 'Everyone says so after all.',
     author: 'thingtwo',
     category: 'react',
@@ -18,8 +18,8 @@ const defaultData = {
   "6ni6ok3ym7mf1p33lnez": {
     id: '6ni6ok3ym7mf1p33lnez',
     timestamp: 1468479767190,
-    title: 'Learn Redux in 10 minutes!',
-    body: 'Just kidding. It takes more than 10 minutes to learn technology.',
+    title: 'Post Another Title here',
+    body: 'Subject',
     author: 'thingone',
     category: 'redux',
     voteScore: -5,
